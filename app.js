39f0a69128647836d6343fb323fb3dcdf8bec2ad
vanjaandrejic <vanjaandrejic5@gmail.com>
2022-08-12 12:12:01 +0200

    create WatchList class, get movie by id

--- app.js
+++ app.js
@@ -8,36 +8,62 @@ const recomendedBtn = document.querySelector(".recomendedBtn");
 console.log(data);
 
 class UI {
-    static displayAllMovies = () => {
-        content.innerHTML = "";
-        data.forEach((movie) => UI.listMovies(movie));
-    };
+  static displayAllMovies = () => {
+    content.innerHTML = "";
+    data.forEach((movie) => UI.listMovies(movie));
+  };
 
-    static displayRecomendedMovies = () => {
-        content.innerHTML = "";
-            data.forEach((movie) => {
-                let rate = movie.imdbRating;
-                if (rate > 7) {
-                UI.listMovies(movie);
-                }
-            });
-    }
+  static displayRecomendedMovies = () => {
+    content.innerHTML = "";
+    data.forEach((movie) => {
+      let rate = movie.imdbRating;
+      if (rate > 7) {
+        UI.listMovies(movie);
+      }
+    });
+  };
 
-    static listMovies = (movie) => {
-        content.innerHTML += `<div class="card col-md-3 col-sm-12 m-3">
+  static listMovies = (movie) => {
+    content.innerHTML += `<div class="card col-md-3 col-sm-12 m-3">
                             <div class="m-3">
                               <h3>${movie.title}</h3>
                               <h4>${movie.year}</h4>
                               <h5>${movie.genres}</h5>
                             </div>
               
-                            <button data-id="${movie.id}" class="btn btn-primary watchBtn mt-4">Add to Watch List</button>`
+                            
+                            <div class="dropdown">
+                              <button data-id="${movie.id}" class="btn btn-primary watchBtn dropdown-toggle m-4" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              Add to Watch List
+                              </button>
+                              <ul class="dropdown-menu bg-secondary">
+                                <li><a class="dropdown-item" href="#">Monday</a></li>
+                                <li><a class="dropdown-item" href="#">Thuesday</a></li>
+                                <li><a class="dropdown-item" href="#">Wednesday</a></li>
+                                <li><a class="dropdown-item" href="#">Thursday</a></li>
+                                <li><a class="dropdown-item" href="#">Friday</a></li>
+                                <li><a class="dropdown-item" href="#">Saturday</a></li>
+                                <li><a class="dropdown-item" href="#">Sunday</a></li>
+                              </ul>
+                            </div>`;
+  };
+}
+
+class WatchList {
+    static addToWatch = (e) => {
+        let id = e.target.dataset.id;
+        let movie = data.filter((movie)=> movie.id == id)[0];
+        console.log(movie);
     }
 }
 
 UI.displayAllMovies();
 
+const watchBtns = document.querySelectorAll(".watchBtn");
+
+watchBtns.forEach((btn) => {
+  btn.onclick = WatchList.addToWatch.bind(WatchList);
+});
+
 allBtn.onclick = UI.displayAllMovies.bind(UI);
 recomendedBtn.onclick = UI.displayRecomendedMovies.bind(UI);
-
-
